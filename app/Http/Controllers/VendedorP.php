5f9e9d9c3bf509e69f076983e6a79ebb9b6dcc29
luisjj12 <luisjmnzjln@gmail.com>
2026-08-11 23:38:52 +0200

    portfolio

--- app/Http/Controllers/VendedorP.php
+++ app/Http/Controllers/VendedorP.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categoria;
+use App\Models\Items_pedido;
 use App\Models\Productos;
 use App\Models\User;
 
@@ -18,9 +19,32 @@ class VendedorP extends Controller
 
 
         if(auth()->user()->rol == "admin"){
-            return view('admin.AdminVendedor', ['productos' => Productos::paginate(7), 'categorias' => Categoria::all()]);
+            return view('admin.AdminVendedor', ['productos' => Productos::with('vendedor')->paginate(7), 'categorias' => Categoria::all()]);
         }else{
-            return view('admin.AdminVendedor',["productos" => $user->productos()->paginate(7), "countP" => $user->productos()->count() ,  "categorias" => Categoria::all()]) ;
+            $idsProductos = $user->productos()->pluck('id');
+
+            $totalVentas = Items_pedido::whereIn('producto_id', $idsProductos)->sum('precio');
+            $totalPedidosVendedor = Items_pedido::whereIn('producto_id', $idsProductos)->count();
+
+            $ventasPorMes = Items_pedido::whereIn('producto_id', $idsProductos)
+                ->selectRaw('MONTH(created_at) as mes, SUM(precio) as total')
+                ->groupBy('mes')
+                ->pluck('total', 'mes')
+                ->toArray();
+
+            $ventaM = [];
+            for ($i = 1; $i <= 12; $i++) {
+                $ventaM[$i] = $ventasPorMes[$i] ?? 0;
+            }
+
+            return view('admin.AdminVendedor', [
+                "productos" => $user->productos()->paginate(7),
+                "countP" => $user->productos()->count(),
+                "categorias" => Categoria::all(),
+                "totalVentas" => $totalVentas,
+                "totalPedidosVendedor" => $totalPedidosVendedor,
+                "ventasPorMes" => $ventaM,
+            ]);
         }
 
     }
